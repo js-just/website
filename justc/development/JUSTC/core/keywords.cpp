19442bc99f14d9a5000572f9e82ce301ff38b210
js-just <support@juststudio.is-a.dev>
2026-06-20 10:34:14 +0000

    Update from https://github.com/js-just/JUSTC/commit/fc629cf791bdccacb64318ad1d71b2e53066b5e4

--- justc/development/JUSTC/core/keywords.cpp
+++ justc/development/JUSTC/core/keywords.cpp
@@ -58,5 +58,6 @@ const std::vector<std::string> keywords = {
     // cpp type keywords
     "int8", "int16", "int32", "int64", "int128",
     "uint8", "uint16", "uint32", "uint64",
-    "uint128", "float32", "float64", "float128"
+    "uint128", "float32", "float64", "float128",
+    "cuint8", "cuint16", "cuint32", "cuint64",
 };
