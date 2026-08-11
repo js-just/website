bbc2deb19514d8becf3b8bb6c65c6db721002735
js-just <support@juststudio.is-a.dev>
2026-08-11 14:39:16 +0000

    Update from https://github.com/justc-lang/JUSTC/commit/1c3a9180b10ae1f6cfc6cc8f45944245b6d422d8

--- justc/development/JUSTC/core/justb.cpp
+++ justc/development/JUSTC/core/justb.cpp
@@ -35,13 +35,13 @@ JUSTB - Just an Ultimate Site Tool Binary file format.
 
 namespace JUSTB {
 
-bool writeHeader(std::ostream& out, const std::string& version) {
+bool writeHeader(std::ostream& out, const std::string& version, const uint8_t& compression, const uint8_t& type) {
     Header header;
     memcpy(header.magic, MAGIC, MAGIC_SIZE);
-    header.filetype = 0;
+    header.filetype = type;
     header.version_len = static_cast<uint8_t>(version.size());
     header.version = version;
-    header.compression = 0;
+    header.compression = compression;
 
     out.write(header.magic, MAGIC_SIZE);
     out.write(reinterpret_cast<const char*>(&header.filetype), sizeof(header.filetype));
@@ -63,9 +63,7 @@ bool readHeader(std::istream& in, Header& header) {
 }
 
 bool validateHeader(const Header& header) {
-    return memcmp(header.magic, MAGIC, MAGIC_SIZE) == 0 &&
-           header.version == JUSTC_VERSION &&
-           header.compression == 0;
+    return memcmp(header.magic, MAGIC, MAGIC_SIZE) == 0;
 }
 
 }
